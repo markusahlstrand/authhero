47f678a906e86ea9e503c9976c7997af1648efbe
Markus Ahlstrand <markus@ahlstrand.de>
2024-07-01 00:51:48 +0200

    fix: add auth

--- apps/react-admin/src/App.tsx
+++ apps/react-admin/src/App.tsx
@@ -1,17 +1,31 @@
-
-import { Admin, Resource, ListGuesser, EditGuesser, ShowGuesser } from 'react-admin';
-import { Layout } from './Layout';
-import { dataProvider } from './dataProvider';
-
+import {
+  Admin,
+  Resource,
+  ListGuesser,
+  EditGuesser,
+  ShowGuesser,
+} from "react-admin";
+import { Layout } from "./Layout";
+import { dataProvider } from "./dataProvider";
+import { authProvider } from "./authProvider";
 
 export const App = () => (
-    <Admin
-        layout={Layout}
-        dataProvider={dataProvider}
-	>
-        <Resource name="posts" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
-		<Resource name="comments" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
-    </Admin>
+  <Admin
+    layout={Layout}
+    dataProvider={dataProvider}
+    authProvider={authProvider}
+  >
+    <Resource
+      name="posts"
+      list={ListGuesser}
+      edit={EditGuesser}
+      show={ShowGuesser}
+    />
+    <Resource
+      name="comments"
+      list={ListGuesser}
+      edit={EditGuesser}
+      show={ShowGuesser}
+    />
+  </Admin>
 );
-
-    
---
+++ apps/react-admin/src/authProvider.ts
@@ -0,0 +1,42 @@
+import { Auth0AuthProvider, httpClient } from "ra-auth-auth0";
+import { Auth0Client } from "@auth0/auth0-spa-js";
+
+const getUrlByEnvironment = () => {
+  // eslint-disable-next-line no-undef
+  const VITE_VERCEL_URL = process.env.VITE_VERCEL_URL;
+
+  if (VITE_VERCEL_URL) {
+    return `https://${VITE_VERCEL_URL}`;
+  }
+
+  // rename this? and nuke VITE_LOGIN_REDIRECT_URL env var
+  return import.meta.env.VITE_LOGOUT_REDIRECT_URL;
+};
+
+export const baseRedirectUri = getUrlByEnvironment();
+
+const auth0 = new Auth0Client({
+  domain: import.meta.env.VITE_AUTH0_DOMAIN,
+  clientId: import.meta.env.VITE_AUTH0_CLIENT_ID,
+  cacheLocation: "localstorage",
+  authorizationParams: {
+    audience: import.meta.env.VITE_AUTH0_AUDIENCE,
+  },
+});
+
+const authProvider = Auth0AuthProvider(auth0, {
+  loginRedirectUri: `${getUrlByEnvironment()}/auth-callback`,
+  logoutRedirectUri: getUrlByEnvironment(),
+});
+
+const authorizedHttpClient = httpClient(auth0);
+
+// Temp debug code
+// function logAccessToken() {
+//   auth0.getTokenSilently().then((token) => {
+//     console.log("Access Token: ", token);
+//   });
+// }
+// logAccessToken();
+
+export { authProvider, authorizedHttpClient };
