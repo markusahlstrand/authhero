2df9c284684c653c1eb544470a2c62c7d5a177c5
Markus Ahlstrand <markus@ahlstrand.de>
2024-07-26 10:39:31 +0200

    fix id for logins table

--- packages/kysely/CHANGELOG.md
+++ packages/kysely/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @authhero/kysely-adapter
 
+## 0.6.0
+
+### Minor Changes
+
+- Fixed issue with incorrect id in logins table
+
 ## 0.5.5
 
 ### Patch Changes
--- packages/kysely/src/logins/get.ts
+++ packages/kysely/src/logins/get.ts
@@ -4,14 +4,15 @@ import { Database } from "../db";
 import { unflattenObject } from "../flattten";
 
 export function get(db: Kysely<Database>) {
-  return async (tenant_id: string, login_id: string): Promise<Login | null> => {
+  return async (_: string, login_id: string): Promise<Login | null> => {
     const now = new Date().toISOString();
 
     const login = await db
       .selectFrom("logins")
-      .where("logins.tenant_id", "=", tenant_id)
+      // TODO: We currentlye don't have a tenant_id in all cases here
+      // .where("logins.tenant_id", "=", tenant_id)
       .where("logins.expires_at", ">", now)
-      .where("logins.id", "=", login_id)
+      .where("logins.login_id", "=", login_id)
       .selectAll()
       .executeTakeFirst();
 
