cb183741215be7a6b30fd9d71a9b6abf6eba4c2a
Markus Ahlstrand <markus@ahlstrand.es>
2025-07-18 13:27:35 +0200

    Add hook for pre user update

--- packages/authhero/src/types/Hooks.ts
+++ packages/authhero/src/types/Hooks.ts
@@ -91,6 +91,18 @@ export type OnExecutePostUserRegistration = (
   api: OnExecutePostUserRegistrationAPI,
 ) => Promise<void>;
 
+export type OnExecutePreUserUpdateAPI = {
+  user: {
+    setUserMetadata: (key: string, value: any) => void;
+  };
+  cancel: () => void;
+};
+
+export type OnExecutePreUserUpdate = (
+  event: HookEvent,
+  api: OnExecutePreUserUpdateAPI,
+) => Promise<void | boolean>;
+
 export type OnExecutePostLoginAPI = {
   prompt: {
     render: (formId: string) => void;
